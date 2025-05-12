1dbcc3617ef57c2b1ffb30a234aad154c22d0424
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-05-12 12:29:50 +0200

    handle loading and empty list cases

--- frontend/src/components/CountriesList.tsx
+++ frontend/src/components/CountriesList.tsx
@@ -2,7 +2,15 @@ import { useCountryStore } from "../store/countryStore";
 import { CountryButton } from "./CountryButton";
 
 export function CountriesList() {
-	const { countries } = useCountryStore();
+	const { countries, isLoading } = useCountryStore();
+
+	if (isLoading) {
+		return <p>Chargement des pays encore...</p>;
+	}
+
+	if (countries.length === 0) {
+		return <p>Aucun pays trouvé.</p>;
+	}
 	return (
 		<div className="countries-list">
 			{countries.map((country) => (

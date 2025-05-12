8ecf75373fb798c8ecccbd47c8b14e1cf45af491
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-05-12 11:03:42 +0200

    create GET_COUNTRIES query

---
+++ frontend/src/api/queries.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const GET_COUNTRIES = gql`
+query Countries {
+  countries {
+    id
+    name
+    code
+    continent {
+      name
+    }
+    emoji
+  }
+}`;
